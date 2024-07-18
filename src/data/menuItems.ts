95cdd5b7129a51dc8885f1325f3e7c706bef9cdb
RodrigoERodriguez <Rodrigoerodriguez48@gmail.com>
2024-07-18 00:43:18 -0300

    Astro - Landing de Hogwarts Legacy - Tutorial Explicativo

---
+++ src/data/menuItems.ts
@@ -0,0 +1,21 @@
+import type { menuItem } from "../types/types";
+
+const menuItems: menuItem[] = [
+    {
+        title: 'Inicio',
+        url: '/',
+        icon: "",
+    },
+    {
+        title: 'Preguntas Frecuentes',
+        url: '/faq',
+        icon: "",
+    },
+    {
+        title: 'Blog',
+        url: '/blog',
+        icon: "",
+    },
+]
+
+export default menuItems;
